953e17a84a70192ef4e811b6bb707d25391461bf
Iago <machado.iago@gmail.com>
2020-06-06 17:20:34 -0300

    chore: add jest and lint staged

--- clean-node-api/jest.config.js
+++ clean-node-api/jest.config.js
@@ -1,14 +1,19 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   roots: ['<rootDir>/src'],
-  // All imported modules in your tests should be mocked automatically
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
-  // The directory where Jest should output its coverage files
-  coverageDirectory: "coverage",
-  // A map from regular expressions to paths to transformers
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/*-protocols.ts',
+    '!**/protocols/**',
+    '!**/test/**'
+  ],
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
-};
+}
